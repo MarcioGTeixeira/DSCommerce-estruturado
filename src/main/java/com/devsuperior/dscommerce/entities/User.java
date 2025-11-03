f7d50169175991e405c9647bb08680a28a78399e
Marcio Teixeira <marcio.soldado@hotmail.com>
2025-11-03 12:54:16 -0300

    Validate self or admin

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -113,6 +113,16 @@ public class User implements UserDetails {
 		return roles;
 	}
 
+	public boolean hasRole(String roleName) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(roleName)) {
+				return true;
+			}
+
+		}
+		return false;
+	}
+
 	public void addRole(Role role) {
 		roles.add(role);
 	}
